224c0afaa50a9c40ff0c486a998b70ad4ab359aa
Babak Pourkazemi <bp5xj@virginia.edu>
2014-11-28 10:51:41 -0500

    Trans sorting

--- Ray/rayGroup.todo.cpp
+++ Ray/rayGroup.todo.cpp
@@ -51,48 +51,6 @@ int RayGroup::getOpenGLCallList(void){
     return glGenLists(1);
 }
 
-/*
-void accFrustum(GLdouble left, GLdouble right, GLdouble bottom,
-        GLdouble top, GLdouble near, GLdouble far, GLdouble pixdx, 
-        GLdouble pixdy, GLdouble eyedx, GLdouble eyedy, 
-        GLdouble focus) {
-    GLdouble xwsize, ywsize; 
-    GLdouble dx, dy;
-    GLint viewport[4];
-
-    glGetIntegerv (GL_VIEWPORT, viewport);
-
-    xwsize = right - left;
-    ywsize = top - bottom;
-    dx = -(pixdx*xwsize/(GLdouble) viewport[2] + 
-            eyedx*near/focus);
-    dy = -(pixdy*ywsize/(GLdouble) viewport[3] + 
-            eyedy*near/focus);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum (left + dx, right + dx, bottom + dy, top + dy, 
-            near, far);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glTranslatef (-eyedx, -eyedy, 0.0);
-}
-
-void accPerspective(GLdouble fovy, GLdouble aspect, 
-        GLdouble near, GLdouble far, GLdouble pixdx, GLdouble pixdy,
-        GLdouble eyedx, GLdouble eyedy, GLdouble focus) {
-    GLdouble fov2,left,right,bottom,top;
-    fov2 = ((fovy*PI_) / 180.0) / 2.0;
-    
-    top = near / (fcos(fov2) / fsin(fov2));
-    bottom = -top;
-    right = top * aspect;
-    left = -right;
-    
-    accFrustum (left, right, bottom, top, near, far,
-            pixdx, pixdy, eyedx, eyedy, focus);
-}
-*/
 
 int RayGroup::drawOpenGL(int materialIndex){
     glEnable( GL_BLEND );
@@ -118,18 +76,22 @@ int RayGroup::drawOpenGL(int materialIndex){
     glDepthMask( GL_FALSE );  // Depth buffer is read-only 
     */
 
-    /*
-    glClear(GL_ACCUM_BUFFER_BIT);
-    for( jitter = 0; jitter < ACSIZE; jitter++ ) {
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        accPerspective( 50.0,
-                (GLdouble) viewport[2]/(GLdouble) viewport[3],
-                1.0, 15.0, j8[jitter].x, j8[jitter].y, 0.0, 0.0, 1.0);
-    */
-        /* Draw objects */
-        // glMatrixMode( GL_MODELVIEW );
-        if ( openGLCallListID == 0 ) {
-            // Draw shapes as before
+    /* Draw objects */
+    // glMatrixMode( GL_MODELVIEW );
+    if ( openGLCallListID == 0 ) {
+        // Draw shapes as before
+        RayShape* curShape;
+        for ( int i = 0; i < sNum; i++ ) {
+            curShape = shapes[i];
+
+            glPushMatrix();
+                glMultMatrixd( (GLdouble *) getMatrix().m );
+                curShape->drawOpenGL( materialIndex );
+            glPopMatrix();
+        }
+    } else {
+        // Draw the call-list using the call-list ID
+        glNewList( openGLCallListID, GL_COMPILE );
             RayShape* curShape;
             for ( int i = 0; i < sNum; i++ ) {
                 curShape = shapes[i];
@@ -139,23 +101,11 @@ int RayGroup::drawOpenGL(int materialIndex){
                     curShape->drawOpenGL( materialIndex );
                 glPopMatrix();
             }
-        } else {
-            // Draw the call-list using the call-list ID
-            glNewList( openGLCallListID, GL_COMPILE );
-                RayShape* curShape;
-                for ( int i = 0; i < sNum; i++ ) {
-                    curShape = shapes[i];
+        glEndList();
 
-                    glPushMatrix();
-                        glMultMatrixd( (GLdouble *) getMatrix().m );
-                        curShape->drawOpenGL( materialIndex );
-                    glPopMatrix();
-                }
-            glEndList();
-
-            glCallList( openGLCallListID );
-        }
-        /* End draw objects */
+        glCallList( openGLCallListID );
+    }
+    /* End draw objects */
 
 
     glDepthMask( GL_TRUE );
