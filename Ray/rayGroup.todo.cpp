184ea62d68d8c09000aa7787ad78467b66fc7374
Babak Pourkazemi <bp5xj@virginia.edu>
2014-11-29 00:55:00 -0500

    Cleaned up a bit

--- Ray/rayGroup.todo.cpp
+++ Ray/rayGroup.todo.cpp
@@ -113,7 +113,6 @@ void RayGroup::drawScene( int materialIndex ) {
             glPushMatrix();
                 glMultMatrixd( (GLdouble *) getMatrix().m );
                 curShape->drawOpenGL( materialIndex );
-                // TODO: Flip here, too?
             glPopMatrix();
         }
     } else {
@@ -136,99 +135,45 @@ void RayGroup::drawScene( int materialIndex ) {
 }
 
 int RayGroup::drawOpenGL(int materialIndex){
+    // First, render scene without reflections
     drawScene(materialIndex);
 
-    glMatrixMode( GL_MODELVIEW );
+    // glMatrixMode( GL_MODELVIEW );
     glClear( GL_STENCIL_BUFFER_BIT );
-
     glEnable( GL_STENCIL_TEST );
         // 1. Set the mirror's pixels to 1 in the stencil buffer
+        // -----------------------------------------------------
         glStencilFunc( GL_ALWAYS, 0x1, 0xFF );
         glStencilOp( GL_KEEP, GL_KEEP, GL_REPLACE );
         glStencilMask(0xFF);
 
-        // Use the mirror to set the stencil buffer, but don't draw it
+        // Use the mirror to set the stencil buffer, but don't draw the mirror
         glColorMask(0,0,0,0);
         glDepthMask(GL_FALSE);
         mirror(1.0);
         glDepthMask(GL_TRUE);
 
         // 2. Reflect the scene, only drawing in the valid stencil area
+        // ------------------------------------------------------------
         glColorMask(1,1,1,1);
         glStencilFunc( GL_EQUAL, 1, 0xFF );
         glStencilMask( 0x00 );  // Disable stencil buffer writing
 
-        // Draw reflection
+        // Reflect the scene 
         glPushMatrix();
             glDisable(GL_CULL_FACE);
             glScalef(-1.0, -1.0, 1.0);
             drawScene( materialIndex );
         glPopMatrix();
         
-        // Draw mirror
-        glDepthMask(GL_FALSE);
+        // Draw mirror's outline
+        // glDepthMask(GL_FALSE);
         mirrorOutline(1.0);
-        // mirror(1.0);
-        glDepthMask(GL_TRUE);
+        // glDepthMask(GL_TRUE);
     glDisable( GL_STENCIL_TEST );
-    mirrorOutline(1.0);
 
-    /*
-    glDisable(GL_CULL_FACE);
-    drawScene( materialIndex );
-    glEnable(GL_CULL_FACE);
-    */
-
-    /*
-    glClearStencil( 0 );
-    glClear( GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
-    glEnable( GL_DEPTH_BUFFER_BIT );
-    glDisable( GL_STENCIL_TEST );
-    glColorMask(0,0,0,0);  // Disable color buffer
-
-    drawScene( materialIndex );
-
-    // Tag mirror's visible pixels with a stencil value 1
-    glEnable( GL_STENCIL_TEST );
-    glStencilOp( GL_KEEP, GL_KEEP, GL_REPLACE );
-    glStencilFunc( GL_ALWAYS, 1, ~0 );
-    mirror( 1.0 );
-
-    // Set depth range to farthest possible value
-    glStencilFunc( GL_EQUAL, 1, ~0 );
-    glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-    */
-    /* End draw objects */
-
-    /*
-    glEnable(GL_STENCIL_TEST);
-        glClearStencil( 0 );
-        glClear( GL_STENCIL_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
-        glStencilFunc( GL_ALWAYS, 0x1, 0xFF );  // Set stencils to 1
-        glStencilOp( GL_KEEP, GL_KEEP, GL_REPLACE );
-        glStencilMask( 0xFF );  // Enable stencil buffer writing
-        glDepthMask(GL_FALSE);
-
-        mirror( 1.0 );
-
-        glStencilFunc(GL_EQUAL, 0x1, 0xFF);  // Test passes if stencil value is 1
-        glStencilMask(0x00);  // Disable stencil buffer writing
-        glDepthMask(GL_TRUE);
-
-        for( int i = 0; i < sNum; i++ ) {
-            RayShape *curShape = shapes[i];
-            // Invert objects about mirror plane
-            glPushMatrix();
-                glDisable(GL_CULL_FACE);
-                glMultMatrixd( (GLdouble *) getMatrix().m );
-                glScalef(1.0, -1.0, 1.0);
-                curShape->drawOpenGL( materialIndex );
-                glEnable(GL_CULL_FACE);
-            glPopMatrix();
-        }
-    glDisable(GL_STENCIL_TEST);
-    */
-
+    // Restore original states
+    glEnable( GL_CULL_FACE );
     glDepthMask( GL_TRUE );
     glDisable(GL_BLEND);
 	return -1; // TODO: return something real
